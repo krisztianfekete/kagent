b5a54d93ff159d6223175935068b3d5cdd0eef01
yashraj S <shuklayashraj68@gmail.com>
2026-09-14 13:04:02 +0000

    fix: discover Kubernetes issuer for kubectl-ate (#2770)
    Fixes the hardcoded Kubernetes issuer in setup/CI by discovering the
    issuer from the cluster’s OIDC config.
    This fixes kubectl-ate auth on kind.
    
    Fixes https://github.com/kagent-dev/kagent/issues/2763
    
    ---------
    
    Signed-off-by: Yashraj Shukla <shuklayashraj68@gmail.com>

--- scripts/setup-cluster/setup-cluster.sh
+++ scripts/setup-cluster/setup-cluster.sh
@@ -58,8 +58,19 @@ actor_id_ca_root="$(kubectl get secret actor-id-ca-pool -n ate-system -o jsonpat
   | openssl x509 -inform der -outform pem)"
 kubectl create secret generic actor-id-ca-certs -n ate-system \
   --from-literal=ca.crt="${actor_id_ca_root}" --dry-run=client -o yaml | kubectl apply -f -
+
+k8s_issuer="$(kubectl get --raw /.well-known/openid-configuration | jq -r '.issuer')"
 kubectl create configmap ate-api-authentication -n ate-system \
-  --from-literal=authentication.yaml=$'actorIdentityJWTProvider: kubernetes\njwtProviders:\n- name: kubernetes\n  issuer: https://kubernetes.default.svc\n  audiences: [api.ate-system.svc]\n  certificateAuthorityFile: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt\n  discoveryTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token\n' \
+  --from-literal=authentication.yaml="$(cat <<EOF
+actorIdentityJWTProvider: kubernetes
+jwtProviders:
+- name: kubernetes
+  issuer: ${k8s_issuer}
+  audiences: [api.ate-system.svc]
+  certificateAuthorityFile: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+  discoveryTokenFile: /var/run/secrets/kubernetes.io/serviceaccount/token
+EOF
+)" \
   --dry-run=client -o yaml | kubectl apply -f -
 
 helm upgrade substrate "oci://ghcr.io/kagent-dev/substrate/helm/substrate" \
